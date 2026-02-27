da91429a3374da3e95252f2b3debd2983874037b
RishabhSharma17 <rishabhsharma17aug2004@gmail.com>
2026-02-27 14:25:21 +0530

    add requirements.txt

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         "Content-Type": "application/json",
     },
